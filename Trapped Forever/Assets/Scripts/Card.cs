7d18901378641a0c38619643400c48e6e152ac4b
Hector Rivera <redmagix2000@gmail.com>
2023-09-23 11:34:23 -0700

    added cards some code doesn

---
+++ Trapped Forever/Assets/Scripts/Card.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+	public bool hasBeenPlayed;
+	public int handIndex;
+
+	GameManager gm;
+
+	private Animator anim;
+	private Animator camAnim;
+
+	public GameObject effect;
+	public GameObject hollowCircle;
+
+	private void Start()
+	{
+		gm = FindObjectOfType<GameManager>();
+	}
+	private void OnMouseDown()
+	{
+		if (!hasBeenPlayed)
+		{
+			Instantiate(hollowCircle, transform.position, Quaternion.identity);
+			
+			camAnim.SetTrigger("shake");
+			anim.SetTrigger("move");
+
+			transform.position += Vector3.up * 3f;
+			hasBeenPlayed = true;
+			gm.availableCardSlots[handIndex] = true;
+			Invoke("MoveToDiscardPile", 2f);
+
+			
+		}
+	}
+
+	void MoveToDiscardPile()
+	{
+		Instantiate(effect, transform.position, Quaternion.identity);
+		gm.discardPile.Add(this);
+		gameObject.SetActive(false);
+	}
+
+
+
+}
--- Trapped Forever/Assets/Scripts/GameManager.cs
+++ Trapped Forever/Assets/Scripts/GameManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<Card> deck = new List<Card>();
+    public Transform[] cardSlots;
+    public bool[] availableSlots;
 
-    // Update is called once per frame
-    void Update()
+    public void DrawCard()
     {
-        
+        if(deck.Count >=1)
+        {
+            Card randCard = deck[Random.Range(0,deck.Count)];
+            for (int i = 0; i < availableSlots.Length; i++)
+            {
+                if(availableSlots[i] == true)
+                {
+                    randCard.gameObject.SetActive(true);
+                    randCard.transform.position = cardSlots[i].position;
+                    availableSlots[i] = false;
+                    deck.Remove(randCard);
+                    return;
+                }
+            }
+        }
     }
+    void Start()
+    {}
+    void Update()
+    {}
 }
